668b6c7c4d9fcf28c1af22345abd2ec8b9c42f41
Nosebone96 <140755097+Nosebone96@users.noreply.github.com>
2023-11-12 21:15:05 -0500

    Menú proncipal

---
+++ Menu_principal.py
@@ -0,0 +1,12 @@
+import customtkinter
+
+customtkinter.set_appearance_mode('System')
+customtkinter.set_default_color_theme('green')
+
+root = customtkinter.CTk()
+root.title('KINNEMA')
+root.geometry('600x400')
+
+
+
+root.mainloop()
